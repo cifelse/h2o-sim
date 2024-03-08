b926c270a85ec0629c107c7199df755ef10d1ef8
cifelse <meetcif@gmail.com>
2024-03-08 14:12:20 +0800

    Add initial files

--- src/models/Hydrogen.java
+++ src/models/Hydrogen.java
@@ -1,5 +1,24 @@
 package models;
 
+import java.io.IOException;
+
 public class Hydrogen {
-    
+    private int hydrogens;
+
+    /**
+     * Default Oxygen Constructor
+     * @param hydrogens
+     */
+    public Hydrogen(int hydrogens) {
+        this.hydrogens = hydrogens;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Console console = new Console();
+
+        int hydrogens = console.input("Enter the number of Oxygen atoms: ").nextInt();
+
+        // Create a new Oxygen
+        new Hydrogen(hydrogens);
+    }
 }
--- src/models/Oxygen.java
+++ src/models/Oxygen.java
@@ -1,5 +1,24 @@
 package models;
 
+import java.io.IOException;
+
 public class Oxygen {
-    
+    private int oxygens;
+
+    /**
+     * Default Oxygen Constructor
+     * @param oxygens
+     */
+    public Oxygen(int oxygens) {
+        this.oxygens = oxygens;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Console console = new Console();
+
+        int oxygens = console.input("Enter the number of Oxygen atoms: ").nextInt();
+
+        // Create a new Oxygen
+        new Oxygen(oxygens);
+    }
 }
