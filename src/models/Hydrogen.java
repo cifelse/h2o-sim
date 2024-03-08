fe35a8391f48406a34958e6e12ca743c8f7fefe4
cifelse <meetcif@gmail.com>
2024-03-09 00:11:53 +0800

    Add Hydrogen and Oxygen clients

--- src/models/Hydrogen.java
+++ src/models/Hydrogen.java
@@ -1,24 +1,69 @@
 package models;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.Socket;
 
-public class Hydrogen {
+public class Hydrogen implements Runnable, Modem {
+    // The Hostname of the Server
+    public static final String HOSTNAME = "localhost";
+
+    // The Number of Hydrogens to send
     private int hydrogens;
+
+    // Create a new Console
+    private Console console;
 
     /**
      * Default Hydrogen Constructor
-     * @param hydrogens
      */
     public Hydrogen(int hydrogens) {
         this.hydrogens = hydrogens;
+        this.console = new Console("Hydrogen Client");
     }
 
+    @Override
+    public void run() {
+        try {
+            Socket socket = new Socket(HOSTNAME, Server.HYDROGEN_PORT);
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            // Receive Connection Confirmation
+            String message = receive(in);
+
+            // Notify the Server how many Hydrogens are there
+            broadcast(out, this.hydrogens);
+
+            // Log all messages sent by the Server until EOM
+            while (!message.contains("EOM")) {
+                console.log(message);
+
+                // Receive the next message
+                message = receive(in);
+            }
+
+            socket.close();
+        }
+        catch (Exception e) {
+            console.log("Server died. Error found: " + e.getMessage());
+        }
+    }
+
+    /**
+     * The Main Method. The Kicker.
+     * @param args - The Command Line Arguments
+     * @throws Exception
+     */
     public static void main(String[] args) throws IOException {
         Console console = new Console();
 
+        // Ask the user for the number of Hydrogens
         int hydrogens = console.input("Enter the number of Hydrogen atoms: ").nextInt();
 
-        // Create a new Hydrogen
-        new Hydrogen(hydrogens);
+        // Create a new Hydrogen Client
+        new Thread(new Hydrogen(hydrogens)).start();
     }
 }
--- src/models/Oxygen.java
+++ src/models/Oxygen.java
@@ -1,24 +1,69 @@
 package models;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.Socket;
 
-public class Oxygen {
+public class Oxygen implements Runnable, Modem {
+    // The Hostname of the Server
+    public static final String HOSTNAME = "localhost";
+
+    // The Number of Oxygens to send
     private int oxygens;
+
+    // Create a new Console
+    private Console console;
 
     /**
      * Default Oxygen Constructor
-     * @param oxygens
      */
     public Oxygen(int oxygens) {
         this.oxygens = oxygens;
+        this.console = new Console("Oxygen Client");
     }
 
+    @Override
+    public void run() {
+        try {
+            Socket socket = new Socket(HOSTNAME, Server.HYDROGEN_PORT);
+
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            // Receive Connection Confirmation
+            String message = receive(in);
+
+            // Notify the Server how many Hydrogens are there
+            broadcast(out, this.oxygens);
+
+            // Log all messages sent by the Server until EOM
+            while (!message.contains("EOM")) {
+                console.log(message);
+
+                // Receive the next message
+                message = receive(in);
+            }
+
+            socket.close();
+        }
+        catch (Exception e) {
+            console.log("Server died. Error found: " + e.getMessage());
+        }
+    }
+
+    /**
+     * The Main Method. The Kicker.
+     * @param args - The Command Line Arguments
+     * @throws Exception
+     */
     public static void main(String[] args) throws IOException {
         Console console = new Console();
 
+        // Ask the user for the number of Oxygens
         int oxygens = console.input("Enter the number of Oxygen atoms: ").nextInt();
 
-        // Create a new Oxygen
-        new Oxygen(oxygens);
+        // Create a new Oxygen Client
+        new Thread(new Oxygen(oxygens)).start();
     }
 }
