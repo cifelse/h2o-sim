3758c472dc9f4cbae238abbb55c0118d55945073
cifelse <meetcif@gmail.com>
2024-03-12 20:28:36 +0800

    Finish Modem functions

--- src/models/Modem.java
+++ src/models/Modem.java
@@ -2,37 +2,55 @@ package models;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.net.Socket;
 
 public interface Modem {
     /**
      * The method to broadcast a message to the sockets
-     * @param out - The DataOutputStream
-     * @param alias - The alias of the user
+     * @param socket - the socket of the receiver
      * @param message - The message to broadcast
+     * @param alias - The alias of the user
      * @throws Exception
      */
-    default public void broadcast(DataOutputStream out, String alias, String message) throws Exception {
+    default public void broadcast(Socket socket, String message, String alias) throws Exception {
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
         out.writeUTF("[" + alias + "]: " + message);
         out.flush();
     }
 
-    default public void broadcast(DataOutputStream out, String message) throws Exception {
-        out.writeUTF("[Anonymous]: " + message);
-        out.flush();
-    }
-
-    default public void broadcast(DataOutputStream out, int number) throws Exception {
-        out.writeInt(number);
+    /**
+     * The method to broadcast a message to the sockets
+     * @param socket - the socket of the receiver
+     * @param message - The message to broadcast
+     * @throws Exception
+     */
+    default public void broadcast(Socket socket, String message) throws Exception {
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        out.writeUTF(message);
         out.flush();
     }
 
     /**
-     * The method to receive a message from the sockets
-     * @param in - The DataInputStream
-     * @return Byte
+     * Request an Element to be bonded to the Server
+     * @param socket - the socket of the server
+     * @param element - The Element
      * @throws Exception
      */
-    default public String receive(DataInputStream in) throws Exception {
+    default public void request(Socket socket, String element) throws Exception {
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        out.writeUTF(element);
+        out.flush();
+    }
+
+    /**
+     * Receive the Element requested by the Client
+     * @param socket - the socket of the requester
+     * @return the Element name
+     * @throws Exception
+     */
+    default public String receive(Socket socket) throws Exception {
+        DataInputStream in = new DataInputStream(socket.getInputStream());
         return in.readUTF();
     }
 }
+
