49037b5f98b913a9ef80eba4f060a1a041b4223f
cifelse <meetcif@gmail.com>
2024-03-12 22:14:29 +0800

    Apply independent bond for Element

--- src/models/Element.java
+++ src/models/Element.java
@@ -1,28 +1,42 @@
 package models;
 
+import java.net.Socket;
+
 /**
  * A special type of ArrayList made for Elements or Atoms
  */
-public class Element {
+public class Element implements Modem {
     // Element Symbol
     private final String name;
-    // Bonded Status
-    private boolean isBonded;
 
-    public Element(String name) {
+    // The Requester
+    private Socket requester;
+
+    /**
+     * Create an element with its name and requesters
+     * @param name
+     */
+    public Element(String name, Socket requester) {
         this.name = name;
-        this.isBonded = false;
+        this.requester = requester;
     }
 
     public String getName() {
         return this.name;
     }
 
-    public boolean isBonded() {
-        return this.isBonded;
+    public Socket getRequester() {
+        return this.requester;
     }
 
-    public void bond() {
-        this.isBonded = true;
+    public String bond(String timestamp) {
+        String message = "[Server]: " + name + ", bonded, " + timestamp;
+        try {
+            broadcast(requester, message);
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        return message;
     }
 }
